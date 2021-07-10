cb3cda6b936932cf8a60c44aa6652db5f2ad74d5
AP <porwal.1@iitj.ac.in>
2021-07-10 15:18:29 +0530

    Stage 1 Completed

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,6 @@
 let mainLocationsDiv = document.getElementById("mainLocationsDiv");
 let mainSensorsDiv = document.getElementById("mainSensorsDiv");
+let navbarLocationsDiv = document.getElementById('navbarLocationsDiv');
 
 const giveTemplate = (data) => {
 
@@ -11,7 +12,6 @@ const giveTemplate = (data) => {
     weight
   }) => {
 
-    const state = location[0];
     const place = location[1];
     mainSensorsDiv.innerHTML = `
             <div class="sensor-data-card" id=${id}>
@@ -40,31 +40,66 @@ const giveTemplate = (data) => {
     div.onclick = function () {
       giveDataOfSelectedLocation(res);
     };
-
     mainLocationsDiv.appendChild(div);
   });
 
   // By default for First element 
   giveDataOfSelectedLocation(data[0]);
 
+  data.forEach((res) => {
+    const { id, location } = res;
+    var div = document.createElement("div");
+    div.className = "location_card";
+    div.tabIndex = 1;
+    div.id = id;
+
+    const state = location[0];
+    const place = location[1];
+
+    div.innerHTML = `<h3>${place}</h3><p>${state}</p>`;
+
+    div.onclick = function () {
+      giveDataOfSelectedLocation(res);
+    };
+
+    navbarLocationsDiv.appendChild(div);
+  });
+
+
 };
 
+function checkedFunc(el) {
+  var body = document.getElementsByTagName("BODY")[0];
+  if (el.checked == true) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "visible";
+  }
+}
+
+
+// Calling API
 async function getRefreshData() {
   try {
     let response = await fetch("/call_data");
     let data = await response.json();
-    console.log(data)
+    // console.log(data)
     giveTemplate(data);
   } catch (err) {
     console.log(err);
   }
 }
 
+
+// Getting Refresh Data in every 2 minutes
 getRefreshData();
 
 setInterval(() => {
+
   // Making previous data empty , then we will store new data coming from backend
   mainLocationsDiv.innerHTML = "";
+  navbarLocationsDiv.innerHTML = "";
   mainSensorsDiv.innerHTML = `<div class="loader"></div>`
+
   getRefreshData();
-}, 2 * 60 * 1000);
+}, 2*60 * 1000);
