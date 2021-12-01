da10340c73baefcc21981a0b43c67325e51f9cf3
Aryan Garg <garg2001aryan@gmail.com>
2021-12-01 16:36:07 +0530

    Final Commit

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ app.get("*", (req, res) => {
   return res.redirect("/dashboard");
 });
 // console.log(process.env.PORT)
-const PORT = process.env.PORT ;
-console.log(PORT)
+const PORT = process.env.PORT || 5000 ;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
